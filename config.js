a5d041739f22fa15aba3f6ab6ca697c255aa0a3e
abdalhay5 <Rpgabdalhay@gmail.com>
2025-02-17 14:53:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  token: "MTM0MDc2NDM4MDcyMzI4NjA0OA.GE6hof.z0X0ZhSOPONfPzGxdIIE01sgTFrX78MG_W3qKk",
+  token: "MTM0MDc2NDM4MDcyMzI4NjA0OA.Gi1hH6.X_r0n6cZiz4NUQtKIxtBLaHs3QJ-UdOhT34URI",
   prefix: "!",
   owner: "1249464472301736144"
 }; 
